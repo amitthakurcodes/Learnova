1b7ae3b8138bf068aa0cf617272abe4d61295853
Amit Thakur <thakuramitchand404@gmail.com>
2026-04-16 01:42:23 +0530

    fix: added EC2 IP in CORS origin

--- Learnova-An-Online-Learning-Platform/server/index.js
+++ Learnova-An-Online-Learning-Platform/server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "http://localhost:3001"], 
+		origin: ["http://localhost:3000", "http://localhost:3001", "http://13.235.2.59"], 
 		credentials:true,
 	})
 )
